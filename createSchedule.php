7913b57e43e6aa09fa41d2a242c9053e48ed02d3
Sagar <sagar0327@gmail.com>
2014-01-17 00:36:49 +0530

    added

--- createSchedule.php
+++ createSchedule.php
@@ -157,3 +157,18 @@ else
        </form>
     </body>
 </html>
+<script>var $selects = $('select');
+$selects.on('change', function() {
+    $("option", $selects).prop("disabled", false);
+    $selects.each(function() {
+        var $select = $(this), 
+            $options = $selects.not($select).find('option'),
+            selectedText = $select.children('option:selected').text();
+        $options.each(function() {
+            if($(this).text() == selectedText) $(this).prop("disabled", true);
+        });
+    });
+});
+
+$selects.eq(0).trigger('change');
+</script>
